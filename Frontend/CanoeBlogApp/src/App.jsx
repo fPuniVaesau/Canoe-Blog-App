c810daa0bee18c9e17fe0b2bd72a6911f8af1a8e
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-10-05 21:07:41 -0700

    adding component to the main entry point.

--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -1,9 +1,11 @@
-
+import LogoLogin from "../components/header/logoLogin/LogoLogin"
 export default function App() {
  
   return (
     <>
-      
+      <div>
+        <LogoLogin />
+      </div>
     </>
   )
 };
