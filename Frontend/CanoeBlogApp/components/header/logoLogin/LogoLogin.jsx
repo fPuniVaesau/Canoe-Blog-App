8c3b5fef843132ca20cdbc6d74d2e0981c124355
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-10-05 21:04:16 -0700

    creating components for the frontend.

--- Frontend/CanoeBlogApp/components/header/logoLogin/LogoLogin.jsx
+++ Frontend/CanoeBlogApp/components/header/logoLogin/LogoLogin.jsx
@@ -1,7 +1,17 @@
-
-export default function LogoLogin(){
-
-    return(
-        
-    )
+export default function LogoLogin() {
+  return (
+    <>
+      <header>
+        <div>
+          <p>my blog.</p>
+        </div>
+        <nav>
+          <ul>
+            <li>login.</li>
+            <li>register.</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -3,17 +3,7 @@ export default function App() {
  
   return (
     <>
-      <header>
-        <div>
-          <p>my blog.</p>
-        </div>
-        <nav>
-          <ul>
-            <li>login.</li>
-            <li>register.</li>
-          </ul>
-        </nav>
-      </header>
+      
     </>
   )
 };
