5f95051bc393dd9db2297b791ede172dac898f3f
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-10-10 19:17:47 -0700

    working on the welcome page.

--- Frontend/CanoeBlogApp/components/Welcome/Welcome.jsx
+++ Frontend/CanoeBlogApp/components/Welcome/Welcome.jsx
@@ -1,9 +1,11 @@
-import style from "../Welcome/Welcome.module.css";
-export default function Welcome(){
-
-  return(
-    <>
-      <h1>Welcome.</h1>
-    </>
-  )
+import style from '../Welcome/Welcome.module.css';
+export default function Welcome() {
+  return (
+    <div className={style.welcomeContainer}>
+      <h1 className={style.title}>Welcome.</h1>
+      <div className={style.enterBtnContainer}>
+        <a href='homepage'>enter.</a>
+      </div>
+    </div>
+  );
 }
