0c7ae68680b75c5ffcabc9a18caded3e1a0aaf3f
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-10-10 22:37:56 -0700

    working on the welcome page.

--- Frontend/CanoeBlogApp/components/Welcome/Welcome.jsx
+++ Frontend/CanoeBlogApp/components/Welcome/Welcome.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import style from '../Welcome/Welcome.module.css';
 export default function Welcome() {
   return (
     <div className={style.welcomeContainer}>
       <h1 className={style.title}>Welcome.</h1>
       <div className={style.enterBtnContainer}>
-        <a href='homepage'>enter.</a>
+        <Link to='/homepage'>enter.</Link>
       </div>
     </div>
   );
