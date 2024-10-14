29b8c790478124dae2867a88f90bd2a4abb3f343
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-10-13 21:40:22 -0700

    working on the blog post page.

--- Frontend/CanoeBlogApp/src/main.jsx
+++ Frontend/CanoeBlogApp/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
 import Home from './Pages/Home.jsx';
 import ErrorPage from './Pages/ErrorPage.jsx';
 import Login from './Pages/Login.jsx';
@@ -9,28 +9,28 @@ import BlogPostPage from './Pages/BlogPostPage.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element : <App />,
-    errorElement : <ErrorPage />,
-    children : [
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
       {
-        path: "/homepage",
-        element : <Home/>
+        path: '/homepage',
+        element: <Home />,
       },
       {
-        path: "/login",
-        element : <Login />
+        path: '/login',
+        element: <Login />,
       },
       {
-        path: "/blogPosts",
-        element: <BlogPostPage />
-      }
-    ]
-  }
-])
+        path: '/blogPosts',
+        element: <BlogPostPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
