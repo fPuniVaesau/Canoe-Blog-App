8e251ab5b352fce35f307816ea47fd77cd149149
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-10-14 22:48:11 -0700

    working on the login page.

--- Frontend/CanoeBlogApp/components/LoginForm/LoginForm.jsx
+++ Frontend/CanoeBlogApp/components/LoginForm/LoginForm.jsx
@@ -1,43 +1,47 @@
-import { useState } from "react";
-import style from "../LoginForm/LoginForm.module.css";
+import { useState } from 'react';
+import style from '../LoginForm/LoginForm.module.css';
+import canoe3d from '../../Assets/3dCanoeV2.png';
 
-export default function LoginForm(){
-    
-    const [formData, setFormData] = useState({
-    username: "",
-    password: "",
+export default function LoginForm() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
   });
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Form Data submitted: ', formData);
-    setFormData({username: ""});
-  }
+    setFormData({ username: '' });
+  };
 
   return (
     <div className={style.LoginFormConatainer}>
-      <form onSubmit={handleSubmit}>
-        <div className={style.fieldsWrapper}>
-          <input
-            type='text'
-            value={formData.username}
-            name='username'
-            placeholder='username'
-            onChange={handleChange}
-          />
-          <input type='Password' name='password' placeholder='password' />
-        </div>
-
-        <button type='submit'>login.</button>
-      </form>
+      <div className={style.imageWrapper}>
+        <img className={style.image} src={canoe3d} alt='canoe 3d logo' />
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className={style.fieldsWrapper}>
+            <input
+              type='text'
+              value={formData.username}
+              name='username'
+              placeholder='username'
+              onChange={handleChange}
+            />
+            <input type='Password' name='password' placeholder='password' />
+          </div>
+          <button type='submit'>login.</button>
+        </form>
+      </div>
     </div>
   );
 }
