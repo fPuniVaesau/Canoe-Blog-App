181fa624d185ebe7bd59ba51ac0c6724381f1619
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-11-24 18:38:17 -0800

    sending items to the repo.

---
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -0,0 +1,24 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className="text-7xl font-bold">Canoe.</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+        provident enim dolorum illum autem, architecto quo labore illo ullam
+        quidem eaque quaerat sint? Nesciunt optio repellendus qui id, nobis est
+        dolorum fugiat officiis eaque minima nulla sed dicta quo exercitationem
+        facere distinctio doloremque porro voluptatem magnam. Atque, officiis!
+        Magni dolores sint fuga quis doloremque officiis! Aperiam tempora enim
+        delectus amet consectetur, ipsum vitae. Quia eos possimus quaerat, fuga
+        distinctio, sint doloremque ut id quam esse dignissimos corporis numquam
+        repellendus iure blanditiis natus nobis minima neque facere nulla omnis
+        nam fugiat odit tenetur? Non, modi consequatur ad dicta illum suscipit
+        accusamus.
+      </p>
+    </div>
+  );
+}
+
+export default App;
