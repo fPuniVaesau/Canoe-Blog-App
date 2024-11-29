b84ca588f950b09005203ea96dc8e41774be9959
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-11-28 19:09:08 -0800

    working on the home page for the project.

--- Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
+++ Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
@@ -1,8 +1,44 @@
+import styles from "./HomePage.module.css";
 
 export default function HomePage(){
-  return(
+  return (
     <>
-      <h1 className="text-7xl ">canoe</h1>
+      <div>
+        {/* Logo and Navigation */}
+        <div>
+          <div>
+            <a href=''>logo here</a>
+          </div>
+
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <a href=''>Features</a>
+                </li>
+                <li>
+                  <a href=''>Blog</a>
+                </li>
+                <li>
+                  <a href=''>About</a>
+                </li>
+                <li>
+                  <a href=''>Contact</a>
+                </li>
+                <li>
+                  <a href=''>Projects</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        {/* Login and Register */}
+        <div className={styles.buttonWrapper}>
+          <button>Log In</button>
+          <button className={styles.registerBtn}>Register</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
