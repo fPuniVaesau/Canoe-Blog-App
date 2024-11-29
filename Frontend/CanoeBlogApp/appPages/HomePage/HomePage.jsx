fd5072204fb43ad311781e15ae12283dc0380acf
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-11-29 14:36:48 -0800

    working on the home page for the applications.

--- Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
+++ Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import styles from "./HomePage.module.css";
-
+import canoeLogo from "../../canoeResources/CanoeLogoWebV2.png"
 export default function HomePage(){
   return (
     <>
@@ -8,7 +8,9 @@ export default function HomePage(){
         {/* Logo and Navigation */}
         <div className={styles.logoNavbarWrapper}>
           <div>
-            <a href=''>logo here</a>
+            <a href="">
+              <img className={styles.mainLogo} src={canoeLogo} alt="" />
+            </a>
           </div>
 
           <div>
@@ -52,6 +54,11 @@ export default function HomePage(){
         <button className={styles.searchBtn}>
           <svg className={styles.iconSearch}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
         </button>
+      </div>
+
+      {/* blog post preview */}
+      <div>
+
       </div>
     </>
   );
