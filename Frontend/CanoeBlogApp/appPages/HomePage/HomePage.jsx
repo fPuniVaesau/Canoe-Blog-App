f65c012c5a0275386947a3f7600bd3fc0b900919
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-11-30 11:36:14 -0800

    working on the welcome message to the page.

--- Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
+++ Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
-import styles from "./HomePage.module.css";
-import canoeLogo from "../../canoeResources/CanoeLogoWebV2.png"
-export default function HomePage(){
+import styles from './HomePage.module.css';
+import canoeLogo from '../../canoeResources/CanoeLogoWebV2.png';
+export default function HomePage() {
   return (
     <>
       {/* navbar */}
@@ -8,8 +8,12 @@ export default function HomePage(){
         {/* Logo and Navigation */}
         <div className={styles.logoNavbarWrapper}>
           <div>
-            <a href="">
-              <img className={styles.mainLogo} src={canoeLogo} alt="canoe logo" />
+            <a href=''>
+              <img
+                className={styles.mainLogo}
+                src={canoeLogo}
+                alt='canoe logo'
+              />
             </a>
           </div>
 
@@ -50,22 +54,27 @@ export default function HomePage(){
           placeholder='Search'
           className={styles.searchBar}
         />
-       
+
         <button className={styles.searchBtn}>
-          <svg className={styles.iconSearch}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+          <svg
+            className={styles.iconSearch}
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 512 512'
+          >
+            <path d='M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z' />
+          </svg>
         </button>
       </div>
 
       {/* blog home page message */}
-      <div>
-        <h2> Lose site of the shores to discover new sea's.</h2>
-        <h3>What is your journey?</h3>
+      <div className={styles.openingMessageWrapper}>
+          <h2> Lose site of the shores to discover new sea's.
+            <span>what is your journey?</span>
+          </h2>
       </div>
 
       {/* blog post preview */}
-      <div>
-        
-      </div>
+      <div></div>
     </>
   );
 }
