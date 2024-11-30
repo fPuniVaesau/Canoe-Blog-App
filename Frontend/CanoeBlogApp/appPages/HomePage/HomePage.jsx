ad0a2c37f3b37d9b7c4100cd3afca5d0b9d2dbcc
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-11-30 13:02:58 -0800

    working on the blog post section for the page.

--- Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
+++ Frontend/CanoeBlogApp/appPages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import styles from './HomePage.module.css';
 import NavigationBar from '../../appComponents/NavigationBar/NavigationBar';
 import SearchBar from '../../appComponents/SearchBar/SearchBar';
 import OpeningMessage from '../../appComponents/OpeningMessage/OpeningMessage';
+import BlogPost from '../../appComponents/BlogPost/BlogPost';
 
 
 export default function HomePage() {
@@ -17,38 +18,9 @@ export default function HomePage() {
       <OpeningMessage />
 
       {/* blog post preview */}
-      <div className={styles.blogPostWrapper}>
-        <div className={styles.blogPostCardContainer}>
-          <div className={styles.blogImgAndAuthorWrapper}>
-            <img
-
-              src='https://images.unsplash.com/photo-1701979397766-bbd6fdd29743?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RS0tX3BuSWlyRzR8fGVufDB8fHx8fA%3D%3D'
-              alt='blog post image'
-            />
-            <div className={styles.blogAuthorDateWrapper}>
-              <p className={styles.blogAuthor}>filo vaesau</p>
-              <div className={styles.dateAndTimeWrapper}>
-                <p>12/1/2024</p>
-                <p>12:00am</p>
-              </div>
-            </div>
-          </div>
-          {/* blog post discription section */}
-          <div className={styles.blogPostDiscriptionWrapper}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-              error velit iure temporibus nemo nobis ipsa rerum eligendi nostrum
-              quis consectetur fugiat repellendus explicabo soluta est. Sint
-              totam aspernatur dolor? Ut voluptas veniam cupiditate illum, nam
-              eum reprehenderit impedit repellendus eius placeat assumenda nemo
-              pariatur cum alias laudantium atque, quas temporibus incidunt.
-              Laudantium odit, nihil, eligendi necessitatibus doloremque
-              provident cupiditate inventore nisi enim sed obcaecati harum
-              explicabo dolor possimus consequuntur.
-            </p>
-          </div>
-        </div>
-      </div>
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
     </>
   );
 }
