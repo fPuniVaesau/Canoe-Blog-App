7de8901801bf644f15f189f384333329f17068df
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-11-30 19:59:38 -0800

    working on the blog post section for the page.

--- Frontend/CanoeBlogApp/appComponents/BlogPost/BlogPost.jsx
+++ Frontend/CanoeBlogApp/appComponents/BlogPost/BlogPost.jsx
@@ -8,7 +8,7 @@ export default function BlogPost(){
         <div className={styles.blogPostCardContainer}>
           <div className={styles.blogImgAndAuthorWrapper}>
             <img
-
+              className={styles.blogPostImage}
               src='https://images.unsplash.com/photo-1701979397766-bbd6fdd29743?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RS0tX3BuSWlyRzR8fGVufDB8fHx8fA%3D%3D'
               alt='blog post image'
             />
