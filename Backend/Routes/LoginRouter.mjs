fbbb2e60a858985c080fa231da327b15d5a22874
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-01 18:46:54 -0800

    working on the backend.

--- Backend/Routes/LoginRouter.mjs
+++ Backend/Routes/LoginRouter.mjs
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import User from "../MongooseValidations/MongooseSchemas/UserSchema.mjs";
 
 const LoginRouter = Router();
 
-LoginRouter.get("/", (request, response)=>{
-    return response.status(200).send({server: "Filo Vaesau", route: "Login Route"});
+LoginRouter.get("/", async (request, response)=>{
+
+    let allUsers = await User.find();
+    response.status(200).send(allUsers);
+  
+})
+
+LoginRouter.post("/", async (request, response)=>{
+    
 })
 
 export default LoginRouter;
