c86611bbe0acb6ba623f81c3e79e527dc6e53234
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-01 17:24:47 -0800

    working on connection to mongo db database.

--- Backend/backendServer.mjs
+++ Backend/backendServer.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import AllRoutesRouter from "./Routes/Allroutes.mjs";
 
 const app = express();
@@ -9,10 +10,25 @@ const corsOptions = {
     origin: "http://localhost:8000"
 };
 
+const connectionString =
+  "mongodb+srv://filovaesau:585K8168W3Fjhowz@projectcanoe.jcfsi.mongodb.net/?retryWrites=true&w=majority&appName=projectCANOE";
+
+  
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions))
 app.use(AllRoutesRouter);
+
+mongoose.connect(connectionString)
+.then(()=>{
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+})
+.catch((err) => {
+    console.log('unable to connect to the database');
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
