531ecc3f3edb99749123c694841427383e319a14
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-01 16:40:05 -0800

    working on the backend components for the application.

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -1,8 +1,21 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
+const corsOptions = {
+    origin: "http://localhost:8000"
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions))
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
+
+app.get("/", (req, res)=>{
+    res.status(200).send("Server : Filo Vaesau");
+});
