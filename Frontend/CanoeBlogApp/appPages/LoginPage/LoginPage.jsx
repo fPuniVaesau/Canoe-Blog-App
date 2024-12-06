db04efaff7cbc17c4d6f4b54259fe5dd8744d557
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-05 22:43:43 -0800

    working on the login page for the frontend.

---
+++ Frontend/CanoeBlogApp/appPages/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+export default function LoginPage() {
+  return (
+    <>
+      <div>
+        <div>
+          <img src='' alt='' />
+          <h1>canoe.</h1>
+        </div>
+
+        <p>welcome back!</p>
+      </div>
+    </>
+  );
+}
