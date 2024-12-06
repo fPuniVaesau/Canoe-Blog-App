28ce3dce932d837f6b25009f6acdf2063fb7da43
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-05 22:55:07 -0800

    working on the login page for the frontend.

--- Frontend/CanoeBlogApp/appPages/LoginPage/LoginPage.jsx
+++ Frontend/CanoeBlogApp/appPages/LoginPage/LoginPage.jsx
@@ -1,11 +1,12 @@
 import styles from "./LoginPage.module.css";
+import canoe3D from "../../canoeResources/3dCanoeV2.png";
 
 export default function LoginPage() {
   return (
     <>
       <div className={styles.welcomeWrapper}>
         <div>
-          <img src='' alt='' />
+          <img src={canoe3D} alt='canoe logo' />
           <h1>canoe.</h1>
         </div>
 
--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import HomePage from "../appPages/HomePage/HomePage";
+import LoginPage from "../appPages/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <HomePage />
+      {/* <LoginPage /> */}
     </div>
   );
 }
