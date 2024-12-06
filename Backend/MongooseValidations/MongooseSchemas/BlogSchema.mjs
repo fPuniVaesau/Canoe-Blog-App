13d2ec7b5ab59d946577783d0744057caaacd400
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-05 19:10:51 -0800

    working on creating blog post schema to connect to database.

---
+++ Backend/MongooseValidations/MongooseSchemas/BlogSchema.mjs
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose"; 
+
+const blogPostSchema = new Schema({
+  title: {
+    required: true,
+    Types: String
+  },
+  content: {
+    required: true,
+    Types: String
+  }
+
+}, {timestamps: true})
+
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
