ca83a75a846ed6d43d7308b0254dfd739950b917
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-07 11:50:30 -0800

    working on the sign up page for the web application.

--- Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
+++ Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
@@ -27,16 +27,21 @@ export default function SignUpPage(){
             </div>
             <div className={styles.inputFieldWrappers}>
               <label htmlFor="password">Password</label>
-              <input className={styles.fieldInputs} type="password" placeholder="" required />
+              <input className={styles.fieldInputs} type="password" placeholder="Password" required />
             </div>
             <div className={styles.inputFieldWrappers}>
               <label htmlFor="re-enter password">Re-enter Password</label>
               <input className={styles.fieldInputs} type="password" placeholder="Re-enter password" required />
             </div>
+            <div className={styles.signUpButtonWrapper}>
+              <button className={styles.signUpButton}>Create account</button>
+            </div>
           </form>
         </div>
 
-
+        <div className={styles.signupImageWrapper}>
+          <img className={styles.signupImg} src="https://images.unsplash.com/photo-1730472528705-d91d6088b7a9?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
       </div>
     </>
   )
