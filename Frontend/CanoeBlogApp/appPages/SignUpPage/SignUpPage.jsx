2852ac0db4fa53d2e164955e31c24a2fde0af201
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-07 10:11:01 -0800

    working on the sign up page for the web application.

---
+++ Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,17 @@
+
+
+export default function SignUpPage(){
+  return(
+    <>
+      <div>
+        <div>
+          <p>Document your journey</p>
+          <h2>Create new account.</h2>
+        </div>
+
+      </div>
+
+
+    </>
+  )
+};
