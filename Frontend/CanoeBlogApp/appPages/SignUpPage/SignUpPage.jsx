b6316338d41f89609b636317e67d9ddf4b2d08d4
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-07 10:19:16 -0800

    working on the sign up page for the web application.

--- Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
+++ Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
@@ -1,13 +1,26 @@
-
+import styles from "./SignUpPage.module.css"
 
 export default function SignUpPage(){
   return(
     <>
-      <div>
+      <div className={styles.signUpMessageWrapper}>
         <div>
           <p>Document your journey</p>
           <h2>Create new account.</h2>
         </div>
+
+        <form action="sumbit">
+          <div className={styles.nameWrapper}>
+            <div>
+              <label htmlFor="">First name</label>
+              <input type="text" placeholder="Silver" required />
+            </div>
+            <div>
+              <label htmlFor="">Last name</label>
+              <input type="text" placeholder="Surfer" required />
+            </div>
+          </div>
+        </form>
 
       </div>
 
