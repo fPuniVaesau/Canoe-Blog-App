3b360fd2b894b9428e60a409dc0251b165668a13
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-06 19:50:13 -0800

    working on the sign up section for the application.

--- Frontend/CanoeBlogApp/appPages/LoginPage/LoginPage.jsx
+++ Frontend/CanoeBlogApp/appPages/LoginPage/LoginPage.jsx
@@ -10,33 +10,37 @@ export default function LoginPage() {
         </div>
 
         <div className={styles.loginSignUpWrapper}>
-          <h2>canoe.</h2>
-          <p>welcome back!</p>
+          <div>
+            <div className={styles.welcomeMessageWrapper}>
+              <h2>welcome back</h2>
+              <p>Let's enter your details!</p>
+            </div>
 
-          <form className={styles.formWrapper} action=''>
-            <div className={styles.inputFieldWrappers}>
-              <label htmlFor='emailAddress'>Email address</label>
-              <input
-                className={styles.fieldInputs}
-                type='text'
-                name=''
-                id=''
-                placeholder='name@email.com'
-                required
-              />
-            </div>
-            <div className={styles.inputFieldWrappers}>
-              <label htmlFor='Password'>Password</label>
-              <input
-                className={styles.fieldInputs}
-                type='password'
-                name=''
-                id=''
-                placeholder='Password'
-                required
-              />
-            </div>
-          </form>
+            <form className={styles.formWrapper} action='submit'>
+              <div className={styles.inputFieldWrappers}>
+                <label htmlFor='emailAddress'>Email address</label>
+                <input
+                  className={styles.fieldInputs}
+                  type='text'
+                  name=''
+                  id=''
+                  placeholder='name@email.com'
+                  required
+                />
+              </div>
+              <div className={styles.inputFieldWrappers}>
+                <label htmlFor='Password'>Password</label>
+                <input
+                  className={styles.fieldInputs}
+                  type='password'
+                  name=''
+                  id=''
+                  placeholder='Password'
+                  required
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
