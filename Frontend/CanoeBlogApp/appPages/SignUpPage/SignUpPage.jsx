560514c3cf72ee134876e1ff8a6e380893992139
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-07 10:57:35 -0800

    working on the sign up page for the web application.

--- Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
+++ Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
@@ -13,13 +13,25 @@ export default function SignUpPage(){
           <form action="sumbit">
             <div className={styles.nameWrapper}>
               <div className={styles.inputFieldWrappers}>
-                <label htmlFor="">First name</label>
+                <label htmlFor="first name">First name</label>
                 <input className={styles.fieldInputs} type="text" placeholder="Silver" required />
               </div>
               <div className={styles.inputFieldWrappers}>
-                <label htmlFor="">Last name</label>
+                <label htmlFor="last name">Last name</label>
                 <input className={styles.fieldInputs} type="text" placeholder="Surfer" required />
               </div>
+            </div>
+            <div className={styles.inputFieldWrappers}>
+              <label htmlFor="email address">Email address</label>
+              <input className={styles.fieldInputs} type="text" placeholder="emailaddress@example.com" required />
+            </div>
+            <div className={styles.inputFieldWrappers}>
+              <label htmlFor="password">Password</label>
+              <input className={styles.fieldInputs} type="password" placeholder="" required />
+            </div>
+            <div className={styles.inputFieldWrappers}>
+              <label htmlFor="re-enter password">Re-enter Password</label>
+              <input className={styles.fieldInputs} type="password" placeholder="Re-enter password" required />
             </div>
           </form>
         </div>
