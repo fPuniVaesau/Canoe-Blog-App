c133a5468248761fc82483793774ded4e4ec74bd
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-07 10:37:48 -0800

    working on the sign up page for the web application.

--- Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
+++ Frontend/CanoeBlogApp/appPages/SignUpPage/SignUpPage.jsx
@@ -3,28 +3,29 @@ import styles from "./SignUpPage.module.css"
 export default function SignUpPage(){
   return(
     <>
-      <div className={styles.signUpMessageWrapper}>
-        <div>
-          <p>Document your journey</p>
-          <h2>Create new account.</h2>
+      <div className={styles.signUpPageContainer}>
+        <div className={styles.messageFormWrapper}>
+          <div className={styles.signUpMessageWrapper}>
+            <p>Document your journey</p>
+            <h2>Create new account.</h2>
+          </div>
+
+          <form action="sumbit">
+            <div className={styles.nameWrapper}>
+              <div>
+                <label htmlFor="">First name</label>
+                <input type="text" placeholder="Silver" required />
+              </div>
+              <div>
+                <label htmlFor="">Last name</label>
+                <input type="text" placeholder="Surfer" required />
+              </div>
+            </div>
+          </form>
         </div>
 
-        <form action="sumbit">
-          <div className={styles.nameWrapper}>
-            <div>
-              <label htmlFor="">First name</label>
-              <input type="text" placeholder="Silver" required />
-            </div>
-            <div>
-              <label htmlFor="">Last name</label>
-              <input type="text" placeholder="Surfer" required />
-            </div>
-          </div>
-        </form>
 
       </div>
-
-
     </>
   )
 };
--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
 import HomePage from "../appPages/HomePage/HomePage";
 import LoginPage from "../appPages/LoginPage/LoginPage";
+import SignUpPage from "../appPages/SignUpPage/SignUpPage";
 
 function App() {
   return (
     <div className={styles.wrapper}>
       {/* <HomePage /> */}
       <LoginPage />
+      <SignUpPage />
     </div>
   );
 }
