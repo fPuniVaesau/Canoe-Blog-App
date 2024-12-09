6b72f18b11527f99c2c5093c3a92d2325e240e91
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-09 14:41:33 -0800

    working on the registration route for the application.

--- Backend/Routes/Allroutes.mjs
+++ Backend/Routes/Allroutes.mjs
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import LoginRouter from "./LoginRouter.mjs";
+import RegistrationRouter from "./RegisterRouter.mjs";
 
 const AllRoutesRouter = Router();
 
 AllRoutesRouter.use("/api/login", LoginRouter);
+AllRoutesRouter.use("api/register", RegistrationRouter);
 
 export default AllRoutesRouter;
