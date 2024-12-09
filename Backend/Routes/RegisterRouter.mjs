f232281d1158fce55d4acef7bccfd31ec8490976
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-09 14:39:52 -0800

    working on the registration route for the application.

--- Backend/Routes/RegisterRouter.mjs
+++ Backend/Routes/RegisterRouter.mjs
@@ -1,1 +1,8 @@
-import { Router } from "express";
+import { response, Router } from "express";
+
+const RegistrationRouter = Router();
+
+RegistrationRouter.get("/", (request, response) => {
+  response.status(201).send({route: "Registration Router"});
+})
+
