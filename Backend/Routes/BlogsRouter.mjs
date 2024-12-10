41b788b9785d90023e45b587bd87177a296a03ca
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-09 18:46:28 -0800

    working on the blog routes for the application.

--- Backend/Routes/BlogsRouter.mjs
+++ Backend/Routes/BlogsRouter.mjs
@@ -6,5 +6,10 @@ BlogRouter.get("/", (request, response) => {
     response.send({router: "Blog Router"})
 })
 
+BlogRouter.get("/:id", (request, response) => {
+    const {params:{id}} = request
+    return response.send({userID: `${id}`});
+})
+
 export default BlogRouter;
 
