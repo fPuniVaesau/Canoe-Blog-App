4a94d1d7984982abf371a0c89b1dfcacc87e28b5
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-12 20:42:13 -0800

    working on handling the information on inputs.

--- Frontend/CanoeBlogApp/appPages/LoginPage/LoginPage.jsx
+++ Frontend/CanoeBlogApp/appPages/LoginPage/LoginPage.jsx
@@ -5,10 +5,7 @@ import { useState } from "react";
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  let handleChange = ()=> {
-
-  }
+  
 
   return (
     <>
