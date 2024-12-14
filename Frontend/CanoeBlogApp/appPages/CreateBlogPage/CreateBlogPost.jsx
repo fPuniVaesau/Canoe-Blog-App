c0da23a90581623b931613927b0335992a3b533b
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-13 17:39:35 -0800

    working on the create blog post page.

--- Frontend/CanoeBlogApp/appPages/CreateBlogPage/CreateBlogPost.jsx
+++ Frontend/CanoeBlogApp/appPages/CreateBlogPage/CreateBlogPost.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+import axios from "axios"
 
 export default function CreateBlogPost(){
+    const [author, setAuthor] = useState("")
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
 
     return(
         <>
             <h2>Create blog here:</h2>
+            <form action="">
+                <label htmlFor="">author</label>
+                <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)}/>
+            </form>
         </>
     )
 }
--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -2,13 +2,15 @@ import styles from "./App.module.css";
 import HomePage from "../appPages/HomePage/HomePage";
 import LoginPage from "../appPages/LoginPage/LoginPage";
 import SignUpPage from "../appPages/SignUpPage/SignUpPage";
+import CreateBlogPost from "../appPages/CreateBlogPage/CreateBlogPost";
 
 function App() {
   return (
     <div className={styles.wrapper}>
       {/* <HomePage /> */}
       {/* <LoginPage /> */}
-      <SignUpPage />
+      {/* <SignUpPage /> */}
+      <CreateBlogPost />
     </div>
   );
 }
