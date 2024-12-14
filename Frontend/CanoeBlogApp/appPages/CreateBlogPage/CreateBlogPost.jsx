515263c5b2f1234b5229f333d7360eba9c2fcbed
FiloPuni <ftpuni@zonemail.clpccd.edu>
2024-12-13 20:45:24 -0800

    working on contacting the backend server.

--- Frontend/CanoeBlogApp/appPages/CreateBlogPage/CreateBlogPost.jsx
+++ Frontend/CanoeBlogApp/appPages/CreateBlogPage/CreateBlogPost.jsx
@@ -5,12 +5,22 @@ export default function CreateBlogPost(){
     const [author, setAuthor] = useState("")
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        axios.get("https://localhost:8000")
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
 
     return(
         <>
             <h2>Create blog here:</h2>
-            <form action="">
+            <form onSubmit={handleSubmit} action="submit">
                 <label htmlFor="">author</label>
                 <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)}/>
             </form>
