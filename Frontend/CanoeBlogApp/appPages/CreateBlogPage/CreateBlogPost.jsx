9e05180e053370b2410f6504e9c8494a75f0cd5f
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-19 12:53:14 -0800

    working on the connecting the backend server to the frontend to create a new blog post.

--- Frontend/CanoeBlogApp/appPages/CreateBlogPage/CreateBlogPost.jsx
+++ Frontend/CanoeBlogApp/appPages/CreateBlogPage/CreateBlogPost.jsx
@@ -1,78 +1,92 @@
-import styles from "./CreateBlogPost.module.css"
-import { useState } from "react"
-import axios from "axios"
+import styles from './CreateBlogPost.module.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-export default function CreateBlogPost(){
-    const newBlogData = {
-    author: "",
-    title: "",
-    content: ""
-    }
- 
-    const [blogData, setBlogData] = useState(newBlogData)
+export default function CreateBlogPost() {
+  const newBlogData = {
+    author: '',
+    title: '',
+    content: '',
+  };
 
-    const handleChange = (e) => {
-        setBlogData({...blogData, [e.target.name]: e.target.value})
-        console.log(blogData);
-    }
+  const [blogData, setBlogData] = useState(newBlogData);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // making a HTTP request to the backend of the application.
-        axios.post("http://127.0.0.1:8000/api/blogs/new_post", blogData)
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error)
-        })
+  const handleChange = (e) => {
+    setBlogData({ ...blogData, [e.target.name]: e.target.value });
+    console.log(blogData);
+  };
 
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // making a HTTP request to the backend of the application.
+    axios
+      .post('http://127.0.0.1:8000/api/blogs/new_post', blogData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      
+  };
 
-    return(
-        <div className={styles.mainContainer}>
-            <h2>New Post</h2>
-            <form onSubmit={handleSubmit} action="submit">
-                {/* author wrapper */}
-               <div className={styles.titleWrapper}>
-                    <label htmlFor="">author</label>
-                    <input
-                    className={styles.inputStyling}
-                    onChange={(e)=>{handleChange(e)}}
-                    name="author"
-                    value={blogData.value}
-                    type="text" />
-               </div>
-
-                {/* content wrapper */}
-                <div className={styles.BlogContentContainer}>
-                    <div className={styles.titleWrapper}>
-                        <label htmlFor="">title</label>
-                        <input
-                        className={styles.inputStyling}
-                        onChange={(e)=>{handleChange(e)}}
-                        name="title"
-                        value={blogData.title}
-                        type="text" />
-                    </div>
-                    <div className={styles.textAreaWrapper}>
-                        <label htmlFor="">content</label>
-                        <textarea 
-                        onChange={(e)=>{handleChange(e)}}
-                        value={blogData.content}
-                        name="content"
-                        placeholder="Let's get started..."
-                        id=""
-                        className={styles.textAreaStyling}
-                        >
-                        </textarea>
-                    </div>
-                </div>
-                {/* button wrapper */}
-                <div className={styles.buttonWrapper}>
-                    <button onClick={(e)=>{handleSubmit(e)}}>post</button>
-                </div>
-            </form>
+  return (
+    <div className={styles.mainContainer}>
+      <h2>New Post</h2>
+      <form onSubmit={handleSubmit} action='submit'>
+        {/* author wrapper */}
+        <div className={styles.titleWrapper}>
+          <label htmlFor=''>author</label>
+          <input
+            className={styles.inputStyling}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            name='author'
+            value={blogData.value}
+            type='text'
+          />
         </div>
-    )
+
+        {/* content wrapper */}
+        <div className={styles.BlogContentContainer}>
+          <div className={styles.titleWrapper}>
+            <label htmlFor=''>title</label>
+            <input
+              className={styles.inputStyling}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name='title'
+              value={blogData.title}
+              type='text'
+            />
+          </div>
+          <div className={styles.textAreaWrapper}>
+            <label htmlFor=''>content</label>
+            <textarea
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              value={blogData.content}
+              name='content'
+              placeholder="Let's get started..."
+              id=''
+              className={styles.textAreaStyling}
+            ></textarea>
+          </div>
+        </div>
+        {/* button wrapper */}
+        <div className={styles.buttonWrapper}>
+          <button
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            post
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
