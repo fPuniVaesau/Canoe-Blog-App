92ba6b962db6558370bcba55bb97db34c810890c
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-20 11:55:58 -0800

    working on the connecting the backend server to the frontend to create a new blog post.

--- Frontend/CanoeBlogApp/appPages/BlogPostPage/BlogPostPage.jsx
+++ Frontend/CanoeBlogApp/appPages/BlogPostPage/BlogPostPage.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import axios from "axios";
+import styles from "./BlogPostPage.module.css"
 import BlogPostPreviewCard from "../../appComponents/BlogPost/BlogPostPreviewCard"
 
 export default function BlogPostPage(){
 
   return(
-    <div>
+    <div className={styles.OutterBlogContainer}>
         <BlogPostPreviewCard />
     </div>
   )
