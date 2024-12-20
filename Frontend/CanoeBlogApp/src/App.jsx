5d89c78fa7bcf85de8d2ae4a09742ffa1e6a38cb
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-20 11:51:55 -0800

    working on the connecting the backend server to the frontend to create a new blog post.

--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "../appPages/HomePage/HomePage";
 import LoginPage from "../appPages/LoginPage/LoginPage";
 import SignUpPage from "../appPages/SignUpPage/SignUpPage";
 import CreateBlogPost from "../appPages/CreateBlogPage/CreateBlogPost";
+import BlogPostPage from "../appPages/BlogPostPage/BlogPostPage";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <HomePage /> */}
       {/* <LoginPage /> */}
       {/* <SignUpPage /> */}
-      <CreateBlogPost />
+      {/* <CreateBlogPost /> */}
+      <BlogPostPage />
     </div>
   );
 }
