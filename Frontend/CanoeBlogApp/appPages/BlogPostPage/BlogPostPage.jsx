1025344efae1110ee1f0e1493ef7bb8c0e330e65
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-20 14:22:52 -0800

    working on the connecting the backend server to the frontend to create a new blog post.

--- Frontend/CanoeBlogApp/appPages/BlogPostPage/BlogPostPage.jsx
+++ Frontend/CanoeBlogApp/appPages/BlogPostPage/BlogPostPage.jsx
@@ -7,7 +7,38 @@ export default function BlogPostPage(){
 
   return(
     <div className={styles.OutterBlogContainer}>
-        <BlogPostPreviewCard />
+        <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
+         <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
+         <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
+         <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
+         <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
+         <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
+         <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
+         <BlogPostPreviewCard
+          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+          author={"Filo Vaesau"}
+        />
     </div>
   )
 }
