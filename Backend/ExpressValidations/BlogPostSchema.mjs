85883beaa077700a693ba193f42b84a9ca5c58d8
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2024-12-21 11:56:25 -0800

    working on the connecting the backend server to the frontend to create a new blog post.

--- Backend/ExpressValidations/BlogPostSchema.mjs
+++ Backend/ExpressValidations/BlogPostSchema.mjs
@@ -1,7 +1,22 @@
 import { ExpressValidator, validationResult, body, query } from "express-validator"
 
 const BlogPostSchema = {
-  required: true
+  title: {
+    isLength:{
+      options: {
+        min: 3, 
+        max: 70
+      },
+      errorMessage: "the title must be between a min: 3 and max:70 characters long."
+    },
+    required: true,
+    notEmpty: {
+      errorMessage: "the title field can not be empty."
+    }
+
+  }
+  
+  
 }
 
 export default BlogPostSchema;
