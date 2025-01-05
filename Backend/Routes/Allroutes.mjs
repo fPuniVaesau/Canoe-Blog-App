a04e72fa8bc00fc9a9eb31c832fee92c0b9809fb
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2025-01-05 12:12:47 -0800

    working on the connecting the backend server to the frontend to create a new blog post.

--- Backend/Routes/Allroutes.mjs
+++ Backend/Routes/Allroutes.mjs
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import HomePageRouter from "./HomePageRouter.mjs";
 import LoginRouter from "./LoginRouter.mjs";
 import RegistrationRouter from "./RegisterRouter.mjs";
 import BlogRouter from "./BlogsRouter.mjs";
 
 const AllRoutesRouter = Router();
 
+AllRoutesRouter.use("/api/home")
 AllRoutesRouter.use("/api/login", LoginRouter);
 AllRoutesRouter.use("/api/register", RegistrationRouter);
 AllRoutesRouter.use("/api/blogs", BlogRouter);
