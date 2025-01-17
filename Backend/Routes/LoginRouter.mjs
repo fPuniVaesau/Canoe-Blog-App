431af35b3a6514e86ce7a8c74e9deb1a0d1af01d
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-01-16 20:52:31 -0800

    working on login verfications.

--- Backend/Routes/LoginRouter.mjs
+++ Backend/Routes/LoginRouter.mjs
@@ -3,6 +3,21 @@ import { checkSchema, validationResult, matchedData } from "express-validator";
 import User from "../MongooseValidations/MongooseSchemas/UserSchema.mjs";
 import UserLoginSchema from "../ExpressValidations/UserLoginSchema.mjs";
 
+const demoLoginData = [
+    {
+        "username": "filo7",
+        "password": "testpassword"
+    },
+    {
+        "username": "morgan10",
+        "password": "testpassword2"
+    },
+    {
+        "username": "s11",
+        "password": "testpassword3"
+    }
+]
+
 const LoginRouter = Router();
 
 LoginRouter.get("/", async (request, response)=>{
@@ -14,7 +29,27 @@ LoginRouter.get("/", async (request, response)=>{
 
 LoginRouter.post("/", checkSchema(UserLoginSchema), async (request, response)=>{
     const { body } = request;
-    console.log(body)
+    const errorResults = validationResult(request);
+
+    if(!errorResults.isEmpty()) return response.status(401).send({errors: errorResults.array()})
+    
+    const validatedUserCredentials = matchedData(request);
+    
+    const foundUser = demoLoginData.filter(user => {
+        if(user.username === validatedUserCredentials.username){
+            return user
+        }
+    })
+
+    if(!foundUser) return response.status(401).send({error: "uesr can not be found at this time."})
+
+    if(foundUser.password !== validatedUserCredentials.password){
+       return response.status(401).send({error: "password does not match"});
+    }
+    
+    request.session.verfiedCredentials = true;
+    request.session.userCredentials = validatedUserCredentials;
+    console.log(request.session);
     return response.sendStatus(200);
 })
 
