5fcb7df94f8b03580838f44d7f731cac97a98941
Filo Vaesau <filopunivaesau@gmail.com>
2025-01-25 13:49:20 -0800

    working with passport to create authorization.

--- Backend/Routes/HomePageRouter.mjs
+++ Backend/Routes/HomePageRouter.mjs
@@ -7,6 +7,7 @@ HomePageRouter.get("/", (request, response)=>{
   //modifying the session will save the session data and cookies
   //Ideally this will be on the home page route and will prompt when the user initially accesses the application.
   request.session.visited = true;
+
   console.log(request.session);
   console.log(request.session.id)
 
