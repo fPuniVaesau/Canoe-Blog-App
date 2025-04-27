8f202de93ff542832d5a9e8e64a40ee02c9b454e
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-04-27 11:43:31 -0700

    working on login credentials for the blog application app.

--- Backend/Routes/LoginRouter.mjs
+++ Backend/Routes/LoginRouter.mjs
@@ -48,32 +48,4 @@ LoginRouter.post("/authentication", passport.authenticate("local"), (request, re
 });
 
 
-// if login is successful we can modify the session with the users credentials and save session data.
-// LoginRouter.post("/", checkSchema(UserLoginSchema), async (request, response)=>{
-//     const { body } = request;
-//     const errorResults = validationResult(request);
-
-//     if(!errorResults.isEmpty()) return response.status(401).send({errors: errorResults.array()})
-    
-//     const validatedUserCredentials = matchedData(request);
-//     const foundUserName = validatedUserCredentials.username;
-//     const foundPassword = validatedUserCredentials.password;
-//     const foundUser = await User.find({username: foundUserName});
-
-//     console.log(foundUser)
-
-//     if(!foundUser){
-//         return response.status(401).send({error: "uesr can not be found at this time."})
-//     }
-
-//     if(foundUser.password !== foundPassword){
-//         console.log(foundPassword.password);
-//        return response.status(401).send({error: "password does not match"});
-//     }
-
-//     request.session.verfiedCredentials = true;
-//     request.session.userCredentials = foundUser;
-//     console.log(request.session);
-//     return response.sendStatus(200);
-// })
 export default LoginRouter;
