b3bef61a7a57bef9bba2151922d5ceb0f554200e
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-04-28 20:03:28 -0700

    working on creating new user for the database.

--- Backend/Routes/RegisterRouter.mjs
+++ Backend/Routes/RegisterRouter.mjs
@@ -11,18 +11,27 @@ RegistrationRouter.get("/", (request, response) => {
 })
 
 // creating a new user profile, we can save the session data.
-RegistrationRouter.post("/", checkSchema(NewUserSChema), async (request, response)=>{
+RegistrationRouter.post("/new_users", checkSchema(NewUserSChema), async (request, response)=>{
   const errorResults = validationResult(request)
   // checks is there are any errors during the post request, validating through users inputs.
-  if(!errorResults.isEmpty()){
-    return response.status(400).send({credentialsError: errorResults.array()});
+
+  try {
+    const userData = matchedData(request);
+
+    const existingUser = await User.find({username: userData.username});
+    
+    if(existingUser) return response.status(409).send({error: `username of ${userData.username} already exists.`});
+
+    const newUserCreated = await User.create(userData);
+    console.log(userData)
+    return response.status(201).send({userCredentials: userData});    
+  } catch (error) {
+      if (!errorResults.isEmpty()) {
+        return response.status(400).send({ credentialsError: errorResults.array() });
+      }
   }
 
   // if no errors, the good data will be used to create a new user.
-  const userData = matchedData(request);
-  const newUserCreated = await User.create(userData);
-  console.log(userData)
-  return response.status(201).send({userCredentials: userData});
 })
 
 export default RegistrationRouter;
