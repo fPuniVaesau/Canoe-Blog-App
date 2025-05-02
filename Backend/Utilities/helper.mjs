063d52c4db2d7872674b100bcf87372ce0da58e1
Filo Vaesau <filopunivaesau@gmail.com>
2025-05-02 08:46:37 -0700

    working on implementing hashing passwords to save to the database.

--- Backend/Utilities/helper.mjs
+++ Backend/Utilities/helper.mjs
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
+//we are creating a helper function to hash users porvided passowrds before creating and saving to the database.
 export const hashPassword = async (password) => {
   const generatedSaltRounds = await bcrypt.genSalt(saltRounds);
   console.log(generatedSaltRounds);
