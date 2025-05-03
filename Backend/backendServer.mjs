3c5ce6e1f8d13ca4f258a3c0196f07838d6e9960
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-05-03 14:53:10 -0700

    working on the session store.

--- Backend/backendServer.mjs
+++ Backend/backendServer.mjs
@@ -22,6 +22,12 @@ const mongoDBusername = process.env.MONGODB_USER
 const mongoDBpassword = process.env.MONGODB_PW;
 const connectionString =`mongodb+srv://${mongoDBusername}:${mongoDBpassword}@projectcanoe.jcfsi.mongodb.net/?retryWrites=true&w=majority&appName=projectCANOE`;
 
+//Connect-mongo to create persistant session data
+const connectMongoSessionStore = MongoStore.create({
+    mongoUrl : connectionString,
+    collectionName : "session_c"
+})
+
 // middleware 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,10 +37,7 @@ app.use(session({
     resave: false, 
     cookie: {
         maxAge: 60000 * 10 //sets the life of the cookie
-    },
-    store: MongoStore.create({
-        client: mongoose.connection.getClient();
-    })
+    }
 }))
 app.use(passport.initialize()) //used for user authentication
 app.use(passport.session())
