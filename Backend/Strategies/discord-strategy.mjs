4494960c7c2ef81604ba8461e2dbc0de19374d16
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-05-04 12:19:54 -0700

    working on being able to connect the user to the application with discord credentials.

--- Backend/Strategies/discord-strategy.mjs
+++ Backend/Strategies/discord-strategy.mjs
@@ -3,14 +3,14 @@ import { Strategy } from "passport-discord";
 
 const f_clientID = process.env.DISCORD_CLIENTiD;
 const f_discordSecret = process.env.DISCORD_SECRET;
+const f_scopes = ["identify", "guilds"];
 
-passport.use(
-  new Strategy(
+passport.use( new Strategy(
     {
       clientID: f_clientID,
       clientSecret: f_discordSecret,
       callbackURL: "http://127.0.0.1:8000/api/authentication/discord/redirect",
-      //   scope: scopes,
+      scope: f_scopes
     },
     () => {}
   )
