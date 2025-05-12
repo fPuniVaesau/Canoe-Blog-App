e5a2b711e9462a8c91fb3ee68a130f18b1b2445e
Filo Vaesau <filopunivaesau@gmail.com>
2025-05-12 12:12:04 -0700

    working on populating all blogs to thier specific author of the post. Re-working some code in my mongoDB user schema.

--- Backend/MongooseValidations/MongooseSchemas/BlogPostSchema.mjs
+++ Backend/MongooseValidations/MongooseSchemas/BlogPostSchema.mjs
@@ -1,19 +1,14 @@
-import mongoose, { Schema } from "mongoose"; 
+import {mongoose, Schema } from "mongoose"; 
 
 const blogPostSchema = new Schema({
   //since the blog is being created by one user it only takes in the users ObjectId as the author.
-  author:{
-    type: ObjectId,
-    ref: "User"
-  },
+  author: {type: Schema.Types.ObjectId, ref : "User"},
   title: {
-    required: true,
-    types: String
+    types: String,
   },
   content: {
-    required: true,
     types: String
-  }
+  },
 }, {timestamps: true})
 
 const BlogPost = mongoose.model("BlogPost", blogPostSchema);
