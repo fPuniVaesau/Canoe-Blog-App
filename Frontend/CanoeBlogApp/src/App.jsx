18fffd3c5aea8ecf8501cef2f050639c0cc54887
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-05-15 18:19:06 -0700

    working on routing for the frontend of the application.

--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -1,21 +1,30 @@
 import styles from "./App.module.css";
+
+import NavigationBar from "../appComponents/NavigationBar/NavigationBar";
+
 import HomePage from "../appPages/HomePage/HomePage";
 import LoginPage from "../appPages/LoginPage/LoginPage";
 import SignUpPage from "../appPages/SignUpPage/SignUpPage";
 import CreateBlogPost from "../appPages/CreateBlogPage/CreateBlogPost";
 import BlogPostPage from "../appPages/BlogPostPage/BlogPostPage";
 import DevPages from "../appPages/DevPages/DevPage";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      {/* <HomePage /> */}
-      <LoginPage />
-      {/* <SignUpPage /> */}
-      {/* <CreateBlogPost /> */}
-      {/* <BlogPostPage /> */}
+      <NavigationBar />
+      
+      <Routes>
+        <Route path="/api/Home"  />
+        {/* <HomePage /> */}
+        {/* <LoginPage /> */}
+        {/* <SignUpPage /> */}
+        {/* <CreateBlogPost /> */}
+        {/* <BlogPostPage /> */}
 
-      {/* <DevPages></DevPages> */}
+        {/* <DevPages></DevPages> */}
+      </Routes>
     </div>
   );
 }
--- Frontend/CanoeBlogApp/src/main.jsx
+++ Frontend/CanoeBlogApp/src/main.jsx
@@ -1,21 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.jsx'
 import React from 'react';
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import ReactDOM from 'react-dom/client';
+import{ BrowserRouter} from "react-router"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
