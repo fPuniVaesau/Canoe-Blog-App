f3cc87967c0694c7267c1fb7ff51079b85439635
Filo Vaesau <filopunivaesau@gmail.com>
2025-05-17 10:59:02 -0700

    working on the frontend of the application.

--- Frontend/CanoeBlogApp/src/main.jsx
+++ Frontend/CanoeBlogApp/src/main.jsx
@@ -2,7 +2,6 @@ import App from './App.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
