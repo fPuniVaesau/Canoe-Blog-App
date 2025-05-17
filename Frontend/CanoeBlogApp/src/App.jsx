54b123e917caa62ce44ea91c5c20724f6f53f93a
Filo Vaesau <filopunivaesau@gmail.com>
2025-05-17 10:30:58 -0700

    working on the frontend of the application.

--- Frontend/CanoeBlogApp/src/App.jsx
+++ Frontend/CanoeBlogApp/src/App.jsx
@@ -8,23 +8,18 @@ import SignUpPage from "../appPages/SignUpPage/SignUpPage";
 import CreateBlogPost from "../appPages/CreateBlogPage/CreateBlogPost";
 import BlogPostPage from "../appPages/BlogPostPage/BlogPostPage";
 import DevPages from "../appPages/DevPages/DevPage";
-import { Routes, Route } from "react-router";
+
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      <NavigationBar />
-      
-      <Routes>
-        <Route path="/api/Home"  />
-        {/* <HomePage /> */}
-        {/* <LoginPage /> */}
-        {/* <SignUpPage /> */}
-        {/* <CreateBlogPost /> */}
-        {/* <BlogPostPage /> */}
+      <HomePage />
+      {/* <LoginPage /> */}
+      {/* <SignUpPage /> */}
+      {/* <CreateBlogPost /> */}
+      {/* <BlogPostPage /> */}
 
-        {/* <DevPages></DevPages> */}
-      </Routes>
+      {/* <DevPages></DevPages> */}
     </div>
   );
 }
--- Frontend/CanoeBlogApp/src/main.jsx
+++ Frontend/CanoeBlogApp/src/main.jsx
@@ -1,13 +1,13 @@
 import App from './App.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import{ BrowserRouter} from "react-router"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+   <App />
+  </React.StrictMode>
 );
 
