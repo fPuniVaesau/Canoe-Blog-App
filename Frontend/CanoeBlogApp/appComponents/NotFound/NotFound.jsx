ec369e4573a45616e5d8d116535fa47e0a57c8c0
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-05-19 17:34:26 -0700

    adding some routing to the error page.

--- Frontend/CanoeBlogApp/appComponents/NotFound/NotFound.jsx
+++ Frontend/CanoeBlogApp/appComponents/NotFound/NotFound.jsx
@@ -1,9 +1,15 @@
-
+import { useNavigate } from "react-router-dom"
 
 export default function NotFound(){
+
+  const navigate = useNavigate();
+
   return (
     <div>
-     <h1>404 | page not found</h1> 
+     <h1>404 | page not found</h1>
+     <div>
+      <button onClick={()=>{navigate("/")}}>go to homepage</button>
+     </div>
     </div>
   )
 }
