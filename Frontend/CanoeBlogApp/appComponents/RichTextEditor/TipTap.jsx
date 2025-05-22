91141e2db6449cda77389939bf0505f56af29a08
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-05-21 17:09:17 -0700

    working on the rich text editor.

--- Frontend/CanoeBlogApp/appComponents/RichTextEditor/TipTap.jsx
+++ Frontend/CanoeBlogApp/appComponents/RichTextEditor/TipTap.jsx
@@ -1,18 +1,25 @@
 // src/Tiptap.tsx
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import TextAlign from "@tiptap/extension-text-align";
 import styles from "./TipTap.module.css";
 import MenuBar from "./MenuBar/MenuBar";
 
-const extensions = [StarterKit]
+const extensions = [
+  StarterKit,
+  TextAlign.configure({
+    types: ["heading", "paragraph"],
+    defaultAlignment: "left",
+  })
+];
 
 const content = '<p>lets get started.</p>'
 
 export default function Tiptap(){
   const editor = useEditor({
     extensions,
-    content
-  })
+    content,
+  });
 
   return (
     <>
