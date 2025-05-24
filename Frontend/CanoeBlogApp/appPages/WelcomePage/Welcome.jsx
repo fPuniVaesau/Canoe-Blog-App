06ba8b28ea21d577df8a208c8af02d4a8d08b7c6
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-05-23 17:55:40 -0700

    working on the home page.

--- Frontend/CanoeBlogApp/appPages/WelcomePage/Welcome.jsx
+++ Frontend/CanoeBlogApp/appPages/WelcomePage/Welcome.jsx
@@ -1,10 +1,18 @@
 import styles from "./Welcome.module.css";
+import Spline from '@splinetool/react-spline';
+import { NavLink } from "react-router-dom";
+
 
 export default function WelcomePage(){
 
-  return(
+  return (
     <>
-      <h1>Welcome</h1>
+      <div className={styles.container}>
+        <Spline className={styles.splineScene} scene='https://prod.spline.design/jPlVh8CU1I52UfCq/scene.splinecode' />
+        <NavLink className={styles.buttonContainer} to="api/home">
+          <button className={styles.welcomeButton} type="button">Welcome to Canoe</button>
+        </NavLink>
+      </div>
     </>
-  )
+  );
 }
