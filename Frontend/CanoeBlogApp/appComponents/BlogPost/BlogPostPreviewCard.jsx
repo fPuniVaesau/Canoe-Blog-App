c5e66862cb640d9a60e4456660f62e4b73f187d2
Filo Vaesau <filopunivaesau@gmail.com>
2025-05-28 14:10:11 -0700

    making some minor changes.

--- Frontend/CanoeBlogApp/appComponents/BlogPost/BlogPostPreviewCard.jsx
+++ Frontend/CanoeBlogApp/appComponents/BlogPost/BlogPostPreviewCard.jsx
@@ -2,7 +2,7 @@ import styles from './BlogPostPreviewCard.module.css';
 import { useEffect } from 'react';
 import axios from 'axios';
 
-export default function BlogPostPreviewCard({ imgUrl, author, title }) {
+export default function BlogPostPreviewCard({ title, author, content, summary, image, createdAt }) {
 
 
   return (
@@ -12,32 +12,23 @@ export default function BlogPostPreviewCard({ imgUrl, author, title }) {
           <div className={styles.blogImgAndAuthorWrapper}>
             <img
               className={styles.blogPostImage}
-              src={imgUrl}
+              src={image}
               alt='blog post image'
             />
             <div>
-             <h3>{title}</h3>
+             <h3>{prompt.title}</h3>
             </div>
             <div className={styles.blogAuthorDateWrapper}>
               <p className={styles.blogAuthor}>{author}</p>
               <div className={styles.dateAndTimeWrapper}>
-                <p>12/1/2024</p>
-                <p>12:00am</p>
+                <p>{createdAt}</p>
               </div>
             </div>
           </div>
           {/* blog post discription section */}
           <div className={styles.blogPostDiscriptionWrapper}>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-              error velit iure temporibus nemo nobis ipsa rerum eligendi nostrum
-              quis consectetur fugiat repellendus explicabo soluta est. Sint
-              totam aspernatur dolor? Ut voluptas veniam cupiditate illum, nam
-              eum reprehenderit impedit repellendus eius placeat assumenda nemo
-              pariatur cum alias laudantium atque, quas temporibus incidunt.
-              Laudantium odit, nihil, eligendi necessitatibus doloremque
-              provident cupiditate inventore nisi enim sed obcaecati harum
-              explicabo dolor possimus consequuntur.
+              {summary}
             </p>
           </div>
         </div>
--- Frontend/CanoeBlogApp/appPages/BlogPostPage/BlogPostPage.jsx
+++ Frontend/CanoeBlogApp/appPages/BlogPostPage/BlogPostPage.jsx
@@ -26,15 +26,13 @@ export default function BlogPostPage(){
     <div className={styles.OutterBlogContainer}>
         <div>
           {blogPosts.map(post => (<div>
-            <h2>{post.title}</h2>
+            <BlogPostPreviewCard title={post.title} summary={post.summary} createdAt={post.createdAt} author={"Filo Vaesau"} image={post.image}/>
+            {/* <h2>{post.title}</h2>
             <h3>{post.summary}</h3>
-            <p>{post.content}</p>
+            <p>{post.content}</p> */}
           </div>))}
         </div>
-        <BlogPostPreviewCard
-          imgUrl={"https://images.unsplash.com/photo-1726004044534-e10fc99783f4?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-          author={"Filo Vaesau"}
-        />
+      
 
     </div>
   )
