f9fb7636334f28892967e01413e0e9a19a23f2c0
FiloPuni <ftpuni@zonemail.clpccd.edu>
2025-05-29 16:03:07 -0700

    working on the blog application.

--- Frontend/CanoeBlogApp/appPages/SinglePostPage/SinglePost.jsx
+++ Frontend/CanoeBlogApp/appPages/SinglePostPage/SinglePost.jsx
@@ -1,15 +1,36 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import axios from "axios"
+
 export default function SinglePost(){
-  const parameters = useParams()
+  const [singlePost, setSinglePost] = useState("");
+  const baseUrl = "http://localhost:8000"
+  const blogPostEndPoint = "/api/blogs"
+  const {id} = useParams()
+  
+  
   useEffect(()=> {
-    console.log(parameters);
+    const fetchSinglePostData = async () => {
+      const response = await axios.get(`${baseUrl}${blogPostEndPoint}/${id}`)
+      const blogPost = await response.data
+      setSinglePost(blogPost)
+    }
+    
+    fetchSinglePostData();
+    console.log(id);
   }, [])
 
   return (
     <div>
-      <h1>Single Blog Post Page</h1>
+      <div>
+        <img src={`${baseUrl}/${singlePost.image}`} />
+      </div>
+      <div>
+        <h1>{singlePost.title}</h1>
+      </div>
+      <div dangerouslySetInnerHTML={{__html: singlePost.content}}/>
+     
     </div>
-  )
+  );
 }
 
